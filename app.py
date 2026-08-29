2ae241f72c53ed713120beb864fc52d49412c6e0
samvelmkrtchyan2011-beep <samvelmkrtchyan2011@icloud.com>
2026-08-29 18:36:15 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import google.generativeai as genai
 
 st.title("AI chat")
 
-genai.configure(api_key="Ab8RN6J1YIYPwItVUh1de08FEAXtuaGq_H3j3WonDdIOFiK6Hg")
-model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key="ՔՈ_API_KEY")AQ.Ab8RN6L7OXwARLBuLyl0len9qXh-3tGmnhtFMuh1hAC5ajQIhQ
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -14,11 +14,16 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("Գրեք հարցը..."):
-    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    try:
+    response = model.generate_content(prompt)
+
     with st.chat_message("assistant"):
-        response = model.generate_content(prompt)
         st.markdown(response.text)
-        st.session_state.messages.append({"role": "assistant", "content": response.text})
+
+except Exception as e:
+    st.write(type(e))
+    st.write(repr(e))
+    st.error(str(e))
