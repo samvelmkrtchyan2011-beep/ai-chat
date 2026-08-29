25a87f0e0e128fd2047cf8142866844a1f706b8f
samvelmkrtchyan2011-beep <samvelmkrtchyan2011@icloud.com>
2026-08-29 17:06:25 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.title("SAMVEL AI")
+
+# API Key-ը
+genai.configure(api_key="AQ.Ab8RN6J1YIYPwItVUh1de08fEAXtuaGq_H3j")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Գրեք հարցը..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = model.generate_content(prompt)
+        st.markdown(response.text)
+        st.session_state.messages.append({"role": "assistant", "content": response.text})
