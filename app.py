657451e4b62fb6ccab7db0228e97d4f74b1bb08b
samvelmkrtchyan2011-beep <samvelmkrtchyan2011@icloud.com>
2026-08-29 19:21:23 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 
-# Կարգավորում ենք API բանալին
-genai.configureAQ.Ab8RN6KH_cFtOpUbfUJkUo-oamOT5C_D2Q-Y180jRGuVfUot-A
+genai.configure(api_key="AQ.Ab8RN6KH_cFtOpUbfUJKUo-oamOT5C_D2Q-Y180jRGuVfUot-")
 
 st.title("AI chat")
 
@@ -11,22 +10,17 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Արտածում ենք նախորդ հաղորդագրությունները
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Եթե օգտատերը գրում է հաղորդագրություն
 if prompt := st.chat_input("Գրեք հարցը..."):
     with st.chat_message("user"):
         st.markdown(prompt)
     
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    try:
-        response = model.generate_content(prompt)
-        with st.chat_message("assistant"):
-            st.markdown(response.text)
-        st.session_state.messages.append({"role": "assistant", "content": response.text})
-    except Exception as e:
-        st.error(f"Սխալ տեղի ունեցավ՝ {e}")
+    response = model.generate_content(prompt)
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
